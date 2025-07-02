a9b48100c5df542e06b77d411b786ed4ff5be8a6
mazhe <mazhe.zhema@gmail.com>
2025-07-02 15:28:10 +0800

    chore: 清理与同步子模块内容

--- scripts/alias.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-alias anchorpush='git add anchor/**/*.yaml anchor/**/*.mdt anchor/**/*.mdc && git commit -m "feat: push anchors" && git push origin main'
--- scripts/validate_and_commit.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python dump_anchor_meta.py && jq '.[].id' anchor_meta_dump.json | sort | uniq -d
